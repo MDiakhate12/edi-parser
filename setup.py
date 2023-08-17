f59952a6479c8007666c939aa12513eddc87180a
Miguel Angel VENTURA FLORES <ext.mventuraflores@cma-cgm.com>
2023-08-17 18:35:01 +0200

    test new module

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='Pre-processing-CICD',
     version='1',
-    packages=find_packages(include=('main.py', 'modules'))
+    description='Project Pre-processing-CICD',
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(exclude=('tests', 'docs', 'sample'))
 )
