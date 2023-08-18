d1b2e9c73c847bee74044d63255206050e7c3733
Miguel Angel VENTURA FLORES <ext.mventuraflores@cma-cgm.com>
2023-08-18 11:11:39 +0200

    test new module

--- setup.py
+++ setup.py
@@ -8,3 +8,13 @@
 #     zip_safe=False,
 #     packages=find_packages(exclude=('tests', 'docs', 'sample'))
 # )
+
+from distutils.core import setup
+
+setup(
+    name='Pre-processing-CICD',
+    version='1',
+    description='Project Pre-processing-CICD',
+    include_package_data=False,
+    packages=['.']
+)
