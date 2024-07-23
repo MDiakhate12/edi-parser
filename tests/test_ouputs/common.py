7d3deec75bc35293574e008e1027cba4f65aa10a
Christelle Mariame VIGNON <ext.cvignon@cma-cgm.com>
2024-07-23 12:00:09 +0000

    🐛 [OE-143] - avoid NaN in OOG_TOP_MEASURE + update unit tests with new test case for flat racks

--- tests/test_ouputs/common.py
+++ tests/test_ouputs/common.py
@@ -2,14 +2,55 @@ import pytest
 import os
 import pandas as pd
 from pandas.testing import assert_frame_equal
+from tests.utils import get_referential_files_from_s3
+from enum import Enum
 from main import main
 pd.set_option('display.max_columns', None)
 
 
 class CommonTests:
 
-    @pytest.fixture(scope="class")
-    def containers_csv(self) -> pd.DataFrame:
+    MOCK_DATA_PATH = None
+    SIMULATION_FOLDER_PATH = None
+    INTERMEDIATE_PATH = None
+    OUTPUT_PATH = None
+    EXPECTED_TEST_RESULTS_FOLDER_PATH = None
+    EVENT = None
+
+    @pytest.fixture
+    def mock_data(self, sim_id):
+        self.MOCK_DATA_PATH = f"{os.path.dirname(os.path.dirname(__file__))}/mock/test_output"
+        SIMULATION_FOLDER_PATH = f"{self.MOCK_DATA_PATH}/simulations/sim_{sim_id}_local"
+        self.INTERMEDIATE_PATH = f"{SIMULATION_FOLDER_PATH}/intermediate"
+        self.OUTPUT_PATH = f"{SIMULATION_FOLDER_PATH}/out"
+        self.EXPECTED_TEST_RESULTS_FOLDER_PATH = f"{SIMULATION_FOLDER_PATH}/expected"
+        os.makedirs(self.EXPECTED_TEST_RESULTS_FOLDER_PATH, exist_ok=True)
+        self.EVENT = dict(
+            vesselImo = "9454450",
+            port = "CNSHK",
+            description = "testscript",
+            path = self.MOCK_DATA_PATH,
+            simulation_id = f"sim_{sim_id}_local",
+            reusePreviousResults = False,
+            dg_exception_rules = "master"
+        )
+        self.EVENT_POSTPROCESSING = dict(
+            vesselImo = "9454450",
+            port = "CNSHK",
+            description = "testscript",
+            path = self.MOCK_DATA_PATH,
+            simulation_id = f"sim_{sim_id}_local",
+            reusePreviousResults = True,
+            dg_exception_rules = "master"
+        )
+
+        # initialize referential data
+        if not os.path.exists(f"{self.MOCK_DATA_PATH}/referential"):
+            get_referential_files_from_s3(local_simulation_folder=self.MOCK_DATA_PATH, environment="prd")
+
+
+    @pytest.fixture
+    def containers_csv(self, mock_data: None) -> pd.DataFrame:
         try:
             if not os.path.exists(f"{self.INTERMEDIATE_PATH}/containers.csv"):
                 main(self.EVENT, enable_logging=True, log_level="DEBUG")
@@ -18,8 +59,8 @@ class CommonTests:
         else:
             return pd.read_csv(f"{self.INTERMEDIATE_PATH}/containers.csv", sep=";")
     
-    @pytest.fixture(scope="class")
-    def rotation_csv(self) -> pd.DataFrame:
+    @pytest.fixture
+    def rotation_csv(self, mock_data: None) -> pd.DataFrame:
         try:
             if not os.path.exists(f"{self.INTERMEDIATE_PATH}/rotation.csv"):
                 main(self.EVENT, enable_logging=True, log_level="DEBUG")
@@ -28,7 +69,7 @@ class CommonTests:
         else:
             return pd.read_csv(f"{self.INTERMEDIATE_PATH}/rotation.csv", sep=";")
 
-    def test_containers_csv_final_columns(self, containers_csv: pd.DataFrame):
+    def test_containers_csv_final_columns(self, containers_csv: pd.DataFrame, mock_data: None):
         expected_columns = ['Container', 'DGheated', 'DischPort', 'Empty', 'Exclusion', 'Height', 'LoadPort', 'NonReeferAtReefer', 'OOG_AFTWARDS', 'OOG_FORWARD', 'OOG_LEFT', 'OOG_LEFT_MEASURE', 'OOG_RIGHT', 'OOG_RIGHT_MEASURE', 'OOG_TOP', 'OOG_TOP_MEASURE', 'POD_nb', 'POL_nb', 'Revenue', 'Setting', 'Size', 'Slot', 'Stowage', 'Subport', 'Type', 'Weight', 'cDG', 'cType', 'cWeight', 'overstowPort', 'priorityID', 'priorityLevel']
         actual_columns = list(containers_csv.columns)
         expected_columns.sort()
@@ -41,6 +82,9 @@ class CommonTests:
             print(actual_columns)
             raise
     
-    def test_rotation_last_asian_port(self, rotation_csv: pd.DataFrame):
+    def test_rotation_last_asian_port(self, rotation_csv: pd.DataFrame, mock_data: dict):
+        # # expected results initialization: uncomment ONLY when a major change affects the expected output
+        # rotation_csv.to_csv(f"{self.EXPECTED_TEST_RESULTS_FOLDER_PATH}/rotation_last_asian_port.csv", sep=";", index=False)
+
         rotation_csv_expected = pd.read_csv(f"{self.EXPECTED_TEST_RESULTS_FOLDER_PATH}/rotation_last_asian_port.csv", sep=";")
         assert_frame_equal(rotation_csv, rotation_csv_expected)
--- tests/test_ouputs/test_basic.py
+++ tests/test_ouputs/test_basic.py
@@ -3,34 +3,15 @@ import os
 from functools import reduce
 import pandas as pd
 from pandas.testing import assert_frame_equal
-from main import main
-from tests.utils import get_referential_files_from_s3
 from tests.test_ouputs.common import CommonTests
 pd.set_option('display.max_columns', None)
 
 
+@pytest.fixture(scope="module", params=["basic"])
+def sim_id(request: pytest.FixtureRequest):
+    return request.param
+
 class TestOutputBasicSimulation(CommonTests):
-
-    # initialize mock data
-    MOCK_DATA_PATH = f"{os.path.dirname(os.path.dirname(__file__))}/mock/test_output"
-    MOCK_SIMULATION_ID = "basic"
-    SIMULATION_FOLDER_PATH = f"{MOCK_DATA_PATH}/simulations/sim_{MOCK_SIMULATION_ID}_local"
-    INTERMEDIATE_PATH = f"{SIMULATION_FOLDER_PATH}/intermediate"
-    OUTPUT_PATH = f"{SIMULATION_FOLDER_PATH}/out"
-    EXPECTED_TEST_RESULTS_FOLDER_PATH = f"{SIMULATION_FOLDER_PATH}/expected"
-    EVENT = dict(
-        vesselImo = "9454450",
-        port = "CNSHK",
-        description = "testscript",
-        path = MOCK_DATA_PATH,
-        simulation_id = f"sim_{MOCK_SIMULATION_ID}_local",
-        reusePreviousResults = False,
-        dg_exception_rules = "master"
-    )
-
-    # initialize referential data
-    if not os.path.exists(f"{MOCK_DATA_PATH}/referential"):
-        get_referential_files_from_s3(local_simulation_folder=MOCK_DATA_PATH, environment="prd")
 
     def test_containers_csv_oog_dimensions(self, containers_csv: pd.DataFrame):
         # expected containers with OOG
